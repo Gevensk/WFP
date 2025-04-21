d95359851bac231ea83e47715ceea584722298f0
MIRACLE RAPHAEL KHUSUMA <s160422080@student.ubaya.ac.id>
2025-04-21 19:58:36 +0700

    Tess

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,39 +1,39 @@
 @extends('layouts.adminlte4')
 
 @section('title')
-Category
+  Category
 @endsection
 
 @section('content')
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
-<div class="container">
-  <h2>Categories</h2>       
-  <table class="table">
+  <div class="container">
+    <h2>Categories</h2>
+    <table class="table">
     <thead>
       <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Foods</th>
+      <th>#</th>
+      <th>Name.</th>
+      <th>Foods</th>
       </tr>
     </thead>
     <tbody>
-        @foreach ($category as $c)
-          <tr>
-            <td>{{ $c->id }}</td>
-            <td>{{ $c->nama }}</td>
-            <td>
-                <ul>
-                  @foreach($c->foods as $f)
-                      <li>{{ $f->nama }}</li>
-                  @endforeach
-                </ul>
-            </td>
-          </tr>
-        @endforeach
+      @foreach ($category as $c)
+      <tr>
+      <td>{{ $c->id }}</td>
+      <td>{{ $c->nama }}</td>
+      <td>
+      <ul>
+      @foreach($c->foods as $f)
+      <li>{{ $f->nama }}</li>
+    @endforeach
+      </ul>
+      </td>
+      </tr>
+    @endforeach
     </tbody>
-  </table>
-</div>
+    </table>
+  </div>
 @endsection
 
 @include("partials.sidebar")
