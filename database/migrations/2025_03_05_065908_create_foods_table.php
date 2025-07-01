6f1692679cbe7f5d5fbe42a70e4ec875e2ad5a06
Luxeroff <realjynxx@gmail.com>
2025-07-01 15:48:24 +0700

    perbaikan bug foods

--- database/migrations/2025_03_05_065908_create_foods_table.php
+++ database/migrations/2025_03_05_065908_create_foods_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
 
             $table->string('nama', 100);
             $table->string('deskripsi', 1000);
-            $table->float('harga');
+            $table->float('harga', 100, 2);
             $table->enum('porsi', ['kecil', 'sedang', 'besar']);
 
             $table->unsignedBigInteger('category_id');
